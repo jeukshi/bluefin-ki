65ec36e7aac9c7cc9e1f47776fbd2078654ff9b4
jeukshi <orlowd+gh@gmail.com>
2025-01-18 12:48:13 +0100

    Exception example.

--- bluefin-ki/src/Bluefin/KiExceptionExamples.hs
+++ bluefin-ki/src/Bluefin/KiExceptionExamples.hs
@@ -3,6 +3,7 @@ module Bluefin.KiExceptionExamples where
 import Bluefin.Eff
 import Bluefin.Exception
 import Bluefin.Parallel.Exception
+import Control.Monad (when)
 
 runForks :: Either String ()
 runForks = runPureEff do
@@ -26,3 +27,24 @@ runForks = runPureEff do
             pureGet par t2
             -- pureGet par t1
             pure ()
+
+validateList :: [Int] -> Either String [Int]
+validateList xs = runPureEff do
+    try \e -> do
+        when (1 `elem` xs) do throw e "1"
+        when (2 `elem` xs) do throw e "2"
+        when (3 `elem` xs) do throw e "3"
+        return xs
+
+validateListPar :: [Int] -> Either String [Int]
+validateListPar xs = runPureEff do
+    try \(e :: Exception String e) -> do
+        runParallelEx e \par ex -> do
+            t2 <- pureForkEx par ex \p1 ex1 -> do
+                when (2 `elem` xs) do pureThrow p1 ex1 "2"
+            t3 <- pureForkEx par ex \p1 ex1 -> do
+                when (3 `elem` xs) do pureThrow p1 ex1 "3"
+            when (1 `elem` xs) do pureThrow par ex "1"
+            pureGet par t2
+            pureGet par t3
+            return xs
